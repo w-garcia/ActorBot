f16e8938282b662cf8571dbe63814fbd0a005932
w-garcia <wgarcia2013@fau.edu>
2016-11-22 17:34:36 -0500

    Updated chatbot-retreival, added prepare_data logic to response_gen

--- actorbot_server/response_gen.py
+++ actorbot_server/response_gen.py
@@ -1,9 +1,19 @@
-from chatbot_retrieval.udc_predict import retrieve_response
-
+from udc_predict import retrieve_response
+from scripts.prepare_data import prepare_data
+from threading import Thread
 
 def tokenizer_fn(iterator):
     return (x.split(" ") for x in iterator)
 
+
 def generate_response(s):
-    #prepare_data()
-    return retrieve_response(s)
+    if s == 'prepare data':
+        t = Thread(target=prepare_data, args=())
+        t.start()
+        #prepare_data()
+        return 'Preparing data'
+    response = retrieve_response(s)
+    eou_index = response.find('__eou__')
+    if eou_index != -1:
+        response = response[:eou_index]
+    return response
