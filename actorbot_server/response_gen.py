5e0c3ed2f7abb86136bfd813430ae7f1aec1c377
w-garcia <wgarcia2013@fau.edu>
2016-11-22 13:03:02 -0500

    reset actorbot_server folder

--- actorbot_server/response_gen.py
+++
@@ -1,9 +0,0 @@
-from chatbot_retrieval.udc_predict import retrieve_response
-
-
-def tokenizer_fn(iterator):
-    return (x.split(" ") for x in iterator)
-
-def generate_response(s):
-    #prepare_data()
-    return retrieve_response(s)
